1f8a32075175e4e5a679804d7a7f5baadfd80feb
MoragProgrammer <suertobrylle23@gmail.com>
2025-07-04 22:37:27 +0800

    3rd commit

--- database/migrations/2025_07_03_074828_create_posts_table.php
+++ database/migrations/2025_07_03_074828_create_posts_table.php
@@ -16,6 +16,12 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->string('category');
+            $table->text('content');
+            $table->string('slug')->unique();
+            $table->string('image')->nullable();
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
