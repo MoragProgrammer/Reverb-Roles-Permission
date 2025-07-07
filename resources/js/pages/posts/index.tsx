006fe6461b935e93f7e7c3489fac780ad53cc491
MoragProgrammer <suertobrylle23@gmail.com>
2025-07-07 19:29:11 +0800

    6th commit

--- resources/js/pages/posts/index.tsx
+++ resources/js/pages/posts/index.tsx
@@ -1,7 +1,9 @@
+import { Input } from '@/components/ui/input';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import { Search } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -15,7 +17,18 @@ export default function Dashboard() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Posts" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-                Index Posts Page
+                <div className="rounded border p-6 shadow-xl">
+
+                    <div>
+                        <div className="relative">
+                            <Input id={'search'} className="peer ps-9" placeholder="Email" type="email" />
+                            <div className="text-muted-foreground/80 pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 peer-disabled:opacity-50">
+                            <Search size={16} aria-hidden="true" />
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </AppLayout>
     );
