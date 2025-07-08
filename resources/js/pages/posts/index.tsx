d2fb28b8cd5d1817346d1ed868877f88fde742a2
MoragProgrammer <suertobrylle23@gmail.com>
2025-07-08 17:33:20 +0800

    8th commit

--- resources/js/pages/posts/index.tsx
+++ resources/js/pages/posts/index.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Card, CardContent } from '@/components/ui/card';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 import { Search } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -19,15 +22,61 @@ export default function Dashboard() {
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
                 <div className="rounded border p-6 shadow-xl">
 
-                    <div>
-                        <div className="relative">
-                            <Input id={'search'} className="peer ps-9" placeholder="Email" type="email" />
+                    <div className="mb-5 flex item-center justify-between">
+                        <div className="relative w-full sm:w-1/3">
+                            <Input id={'search'} className="peer ps-9" placeholder="Search..." type="search" />
                             <div className="text-muted-foreground/80 pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 peer-disabled:opacity-50">
                             <Search size={16} aria-hidden="true" />
                             </div>
                         </div>
+
+                    <Button>
+                        <Link href="/post/create" prefetch>
+                            Create Post
+                        </Link>
+                    </Button>
+
                     </div>
 
+                    <Card>
+                        <CardContent>
+                            <Table>
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead>#</TableHead>
+                                        <TableHead>Image</TableHead>
+                                        <TableHead>Title</TableHead>
+                                        <TableHead>Content</TableHead>
+                                        <TableHead>Category</TableHead>
+                                        <TableHead>Status</TableHead>
+                                        <TableHead>Action</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+
+                                <TableBody>
+                                    <TableRow>
+                                         <TableHead>1</TableHead>
+                                        <TableHead>1Image</TableHead>
+                                        <TableHead>1Title</TableHead>
+                                        <TableHead>1Content</TableHead>
+                                        <TableHead>1Category</TableHead>
+                                        <TableHead>1Status</TableHead>
+                                        <TableHead>1Action</TableHead>
+                                    </TableRow>
+                                          <TableRow>
+                                         <TableHead>2</TableHead>
+                                        <TableHead>2Image</TableHead>
+                                        <TableHead>2Title</TableHead>
+                                        <TableHead>2Content</TableHead>
+                                        <TableHead>2Category</TableHead>
+                                        <TableHead>2Status</TableHead>
+                                        <TableHead>2Action</TableHead>
+                                    </TableRow>
+                                </TableBody>
+
+                            </Table>
+                        </CardContent>
+                    </Card>
                 </div>
             </div>
         </AppLayout>
